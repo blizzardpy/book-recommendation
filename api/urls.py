7fadb2ccf46d90b2b69472d393712ccace77aabc
Amirkian Kiani <74903450+DarkSnowJR@users.noreply.github.com>
2024-07-21 14:47:06 +0330

    [add] docstring for testcases

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,5 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
