da585636cf551b6145e1edc153a5797c5d5bca30
Amirkian Kiani <74903450+DarkSnowJR@users.noreply.github.com>
2024-07-21 19:36:23 +0330

    [add] login api

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import LoginView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
--- api/views.py
+++ api/views.py
@@ -4,8 +4,7 @@ from rest_framework import status
 from django.db import connection
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
-
-User = get_user_model()
+from .models import User
 
 
 class LoginView(APIView):
@@ -20,8 +19,10 @@ class LoginView(APIView):
             **kwargs: Arbitrary keyword arguments.
 
         Returns:
-            Response: The HTTP response object containing the user information, refresh token, and access token.
-            If the credentials are invalid, an error message is returned.
+            Response:
+                The HTTP response object containing the user information,
+                refresh token, and access token. If the credentials are
+                invalid, an error message is returned.
         """
         # Get the username and password from the request data
         username = request.data.get('username')
@@ -30,24 +31,25 @@ class LoginView(APIView):
         # Check if the username and password are provided
         if not username or not password:
             # Return an error response if the username or password is missing
-            return Response({'error': 'Username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {'error': 'Username and password are required'},
+                status=status.HTTP_400_BAD_REQUEST)
 
         # Query the database for the user credentials
         with connection.cursor() as cursor:
+            # Execute the SQL query to check if the user exists
             cursor.execute(
-                'SELECT id, username, password FROM users WHERE username = %s',
-                [username]
-            )
+                'SELECT id, password FROM users WHERE username = %s', [username])
             row = cursor.fetchone()
 
         # Check if the user exists in the database
         if row is not None:
-            user_id, user_username, user_password = row
+            user_id, user_password = row
 
             # Check if the password is correct
             if password == user_password:
                 # Create a user object with the retrieved information
-                user = User(id=user_id, username=user_username)
+                user = User(id=user_id, username=username)
                 # Generate refresh and access tokens
                 refresh_token = RefreshToken.for_user(user)
                 # Return a success response with the user information, refresh token, and access token
@@ -61,4 +63,6 @@ class LoginView(APIView):
                 }, status=status.HTTP_200_OK)
 
         # Return an error response if the credentials are invalid
-        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(
+            {'error': 'Invalid Credentials'},
+            status=status.HTTP_401_UNAUTHORIZED)
